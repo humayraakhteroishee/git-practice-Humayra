736a6114dbc7111fcd84b9c72b6abec324afac7e
Humayra <utsnrisbh@gmail.com>
2026-08-02 09:49:47 -0500

    Added basic calculator function

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 from datetime import date
+from utils import add, subtract
+
 
 print("Name: Humayra Akhter Oishee")
 print("Today's date:", date.today())
+
+print("(a+b)= ", add(78, 59))
+print("(a-b)= ", subtract(79, 59))
